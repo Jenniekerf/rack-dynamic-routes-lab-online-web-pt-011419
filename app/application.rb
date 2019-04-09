8677d3de52ea7f472f38ec41637f53ec7bff91a8
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-09 22:51:43 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,33 @@
+require "pry"
+
+class Application
+  
+  @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
+  
+  def call(env)
+   resp = Rack::Response.new
+   req = Rack::Request.new(env)
+   
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item.price}"
+      end
+    else 
+    resp.write "Route not found"
+    resp.status = 404
+   
+#     resp.write "Item not found"
+#     resp.status = 400
+    
+ 
+  end
+  #binding.pry
+  
+  # item_names = @@items.collect {|i| i.name }
+  
+  
+  
+   resp.finish 
+  end
+end
