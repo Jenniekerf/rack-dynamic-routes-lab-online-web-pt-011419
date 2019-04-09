cfcbef3e38f3e25e1f638219286233f32c419e10
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-09 23:17:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
    req = Rack::Request.new(env)
    
     
-    if req.path.match(/items/)  #and the requested item exists
+    if req.path.match(/items/)  
     
     x = req.path.split("/items/").last
     
